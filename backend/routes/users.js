54aab28352939417cbc7188dc7cd4976caeb534c
Pronit24 <pronitparida2000@gmail.com>
2024-04-01 02:13:00 +0530

    user register and login

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,14 +1,50 @@
 const express = require("express");
 const router = express.Router();
-// Import any user-related functionalities
+const User = require("../models/User");
+const bcrypt = require("bcrypt")
 
-// Define user-related routes
-router.post("/", (req, res) => {
-  // Handle user creation logic
+//register
+router.post("/register", async (req, res)=>{
+    try {
+        //generate new password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
+        //create new user
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword
+        });
+
+        //save user and send response
+        const user = await newUser.save();
+        res.status(200).json(user._id);
+    } catch(err) {
+        res.status(500).json(err);
+    }
 });
 
-router.get("/", (req, res) => {
-  // Handle user retrieval logic
-});
+//login
+router.post("/login", async (req, res) => {
+    try {
+      //find user
+      const user = await User.findOne({ username: req.body.username });
+      !user && res.status(400).json("Wrong username or password");
+  
+      //validate password
+      const validPassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+      !validPassword && res.status(400).json("Wrong username or password");
+  
+      //send response
+      res.status(200).json({ _id: user._id, username: user.username });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 
 module.exports = router;
